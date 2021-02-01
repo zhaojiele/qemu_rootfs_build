b12ddae9bed8ecaeab02fe60aacb7ba225c1a459
zhaojiele <unclexiaole@gmail.com>
2021-02-01 08:49:23 +0000

    转移qemu rootfs 和内核配置工具, V1.0

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#if use drive which if=ide, set root to /dev/sda1
+#if use drive which if=virtio, set root to /dev/vda1
+#remove ‘-enable-kvm’ if CPU don`t suport KVM
+/usr/bin/qemu-system-x86_64 -nographic -drive file=rootfs.img,format=raw,index=0,if=ide \
+	#-enable-kvm \
+	-kernel arch/x86_64/boot/bzImage \
+	-append "rw console=ttyS0 root=/dev/sda1 init=/usr/bin/bash"
