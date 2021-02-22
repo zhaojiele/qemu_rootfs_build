e5057354e53d2efa4b06f3d64603d21d48bf589c
zhaojiele <unclexiaole@gmail.com>
2021-02-22 12:08:57 +0000

    去掉该注释项，会影响脚本执行

--- run.sh
+++ run.sh
@@ -4,6 +4,5 @@
 #if use drive which if=virtio, set root to /dev/vda1
 #remove ‘-enable-kvm’ if CPU don`t suport KVM
 /usr/bin/qemu-system-x86_64 -nographic -drive file=rootfs.img,format=raw,index=0,if=ide \
-	#-enable-kvm \
 	-kernel arch/x86_64/boot/bzImage \
 	-append "rw console=ttyS0 root=/dev/sda1 init=/usr/bin/bash"
